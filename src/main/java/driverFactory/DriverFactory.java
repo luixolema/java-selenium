785f9e73eb8acf742af5144346567e78acedbe9d
Luis Ernesto Macias Avila <bootcamp04@tabit-gmbh.de>
2020-05-27 15:00:32 +0200

    first test with jenkins

--- src/main/java/driverFactory/DriverFactory.java
+++ src/main/java/driverFactory/DriverFactory.java
@@ -16,21 +16,33 @@ import java.util.concurrent.TimeUnit;
 
 public class DriverFactory {
 
-    private static RemoteWebDriver buildRemoteWebDriver(@Nullable String browserType) throws Exception {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName(Optional.ofNullable(browserType).orElse(AppConfig.DEFAULT_BROWSER));
+    private static RemoteWebDriver buildRemoteWebDriver(@Nullable DesiredCapabilities capabilities) throws Exception {
+        capabilities = Optional.ofNullable(capabilities).orElse(DriverFactory.defaultDesiredCapabilities());
+
         RemoteWebDriver driver = new RemoteWebDriver(new URL(AppConfig.REMOTE_HUB_URL), capabilities );
         driver.manage().timeouts().implicitlyWait(AppConfig.IMPLICIT_WAIT, TimeUnit.SECONDS);
 
         return driver;
     }
 
-    public static WebDriver getWebDriver(String browserType) throws Exception {
-        return DriverFactory.buildRemoteWebDriver(browserType);
+    private static DesiredCapabilities defaultDesiredCapabilities() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setBrowserName(AppConfig.DEFAULT_BROWSER);
+
+        return  capabilities;
     }
 
-    public static WebDriver getWebDriver() throws Exception {
-        return DriverFactory.buildRemoteWebDriver(null);
+    public static WebDriver getWebDriver(DesiredCapabilities capabilities) throws Exception {
+        return DriverFactory.buildRemoteWebDriver(capabilities);
+    }
+
+    public static WebDriver getWebDriver() {
+        try {
+            return DriverFactory.buildRemoteWebDriver(null);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
     public static LocalStorage getLocalStorage(WebDriver driver){
--- src/test/java/utils/PageTest.java
+++ src/test/java/utils/PageTest.java
@@ -3,11 +3,13 @@ package utils;
 import config.AppConfig;
 import driverFactory.DriverFactory;
 import org.apache.log4j.Logger;
+import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.*;
 import pages.Page;
 
 import java.util.List;
@@ -17,10 +19,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 public abstract class PageTest {
     protected WebDriver driver;
     protected Logger logger = Logger.getLogger(PageTest.class);
+    private DesiredCapabilities capabilities;
+
+    @BeforeTest
+    @Parameters({"browser"})
+    public void configCapabilities(String browser){
+        capabilities = new DesiredCapabilities();
+
+        switch (browser.toLowerCase()){
+            case BrowserType.CHROME:
+            case BrowserType.FIREFOX: {
+                break;
+            } default:{
+                Assertions.fail("The required browser is not valid");
+            }
+        }
+
+        capabilities.setBrowserName(browser);
+    }
+
+
 
     @BeforeMethod
     public void setUp() throws Exception {
-        driver = DriverFactory.getWebDriver();
+        driver = DriverFactory.getWebDriver(capabilities);
         logger.debug("Created new webDriver");
     }
 
